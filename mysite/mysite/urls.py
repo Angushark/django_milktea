8e9132728c2c06fd66ab86cacc164cc260b4c282
Angushark <Angushark>
2025-12-14 10:03:02 +0800

    improvement

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,7 +4,6 @@ URL configuration for mysite project.
 from django.contrib import admin
 from django.urls import path, include
 from polls import views as polls_views
-from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -22,20 +21,6 @@ urlpatterns = [
     path('register/', polls_views.register, name='register'),
     path('login/', polls_views.user_login, name='login'),
     path('logout/', polls_views.user_logout, name='logout'),
-
-    # 密碼重設
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(template_name='polls/password_reset.html'),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='polls/password_reset_done.html'),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='polls/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='polls/password_reset_complete.html'),
-         name='password_reset_complete'),
 
     # 收藏功能
     path('favorites/', polls_views.favorites_list, name='favorites_list'),
